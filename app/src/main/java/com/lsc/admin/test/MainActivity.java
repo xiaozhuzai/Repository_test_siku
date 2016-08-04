b1f83e7536cf27c2cc2aece9fa9b45050e2b0bdf
xiaozhuzai <1195248542@qq.com>
2016-08-04 19:40:14 +0800

    qwer第三次

--- app/src/main/java/com/lsc/admin/test/MainActivity.java
+++ app/src/main/java/com/lsc/admin/test/MainActivity.java
@@ -3,11 +3,13 @@ package com.lsc.admin.test;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button btn;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
