58996d058252157556f908fa0790e7ea2f871646
xiaozhuzai <1195248542@qq.com>
2016-08-04 19:34:40 +0800

    qwer第二次

--- app/src/main/java/com/lsc/admin/test/MainActivity.java
+++ app/src/main/java/com/lsc/admin/test/MainActivity.java
@@ -1,10 +1,13 @@
 package com.lsc.admin.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
